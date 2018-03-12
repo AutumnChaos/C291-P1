a97a0c229355dc12b0ad19866e357ec688c0261e
Nik <nik@teter.ski>
2018-03-12 17:18:23 -0600

    supervisor role almost 1/3 complete

--- miniproj1.py
+++ miniproj1.py
@@ -7,6 +7,7 @@ import os
 connection = None
 cursor = None
 user = None
+role = None
 
 def connect(path):
     #taken from week 8 lab.
@@ -39,10 +40,107 @@ def hashPassword(password):
     iterations = 100000
 
     return pbkdf2_hmac(hash_name, bytearray(password, 'ascii'), bytearray(salt, 'ascii'), iterations)
+    
+def supervisor_newMasterAccount():
 
-def login_accept():
+    #Select account manager
+
+    #Select all personnel with matching supervisor_pid and who are account managers
+    sqlcmd = "SELECT pid FROM personnel WHERE supervisor_pid=:user"
+    cursor.execute(sqlcmd, {"user":user})
+    supervised = cursor.fetchall()
+    for i in range(0, len(supervised)):
+        supervised[i] = supervised[i][0]
+
+    #Selecting an account manager    
+    while True:
+        printScreen('Supervised managers')
+        for i in range(0, len(supervised)):
+            print(supervised[i])
+        manager = input("Select an account manager:\n\n")
+        if manager in supervised:
+            break
+    
+
+    #Generate unique random for account_no
+    while True:
+        account_no = str(randint(0,sys.maxsize))
+        sqlcmd = "SELECT account_no FROM accounts WHERE account_no=:account_no"
+        cursor.execute(sqlcmd, {"account_no":account_no})
+        try:
+            cursor.fetchone()[0]
+        except:
+            break
+
+    #Retrieve account information
+    customer_name = input("Customer name: ")
+    contact_info = input("Contact information: ")
+    
+    while True:
+        cmd = input("Customer type:\n1.Municipal\n2.Commercial\n3.Industrial\n4.Residential\n\n")
+        if cmd == '1':
+            customer_type = 'municipal'
+            break
+        if cmd == '2':
+            customer_type = 'commercial'
+            break
+        if cmd == '3':
+            customer_type = 'industrial'
+            break
+        if cmd == '4':
+            customer_type = 'residential'
+            break
+        
+    #start_date
+    #end_date
+
+    total_ammount = input("Total ammout for all services : $")
+
+    #Create account
+
+def supervisor_customerReport():
+    pass
+
+def supervisor_managersReport():
+    pass
+
+
+def supervisorActivity():
+    printScreen('Supervisor Actions')
+    cmd = input("1.Create new master account\n2.Create summary report for customer\n3.Create summary report for each supervised account manager\n\n")
+    while True:
+        if cmd == '1':
+            supervisor_newMasterAccount()
+            break
+        if cmd == '2':
+            supervisor_customerReport()
+            break
+        if cmd == '3':
+            supervisor_managersReport()
+            break
+        
+
+def applyRole():
+
+    #DEBUG!!! REMOVE LATER
+    #setting user_id to an existing id in the database.
+    global user
+    user = '74569'
+
+    if role == "Supervisor":
+        supervisorActivity()
+
+
+def login_accept( ):
+    
+    global role
+
     user = cursor.fetchone()[0]
+    sqlcmd = "SELECT role FROM users WHERE user_id=:user_id"
+    cursor.execute(sqlcmd, {"user_id":user})
+    role = cursor.fetchone()[0]
     printScreen('Welcome User' +  user + '\n')
+    applyRole()
 
 def login_deny():
     while True:
@@ -52,6 +150,7 @@ def login_deny():
             login()
             return
         if cmd == '2':
+            main()
             return
 
 def login():
@@ -60,21 +159,20 @@ def login():
         
     printScreen('Login')
     username = input('Username: ')
-    userpass = hashPassword(input('Password: '))
+    password = hashPassword(input('Password: '))
 
     sqlcmd = "SELECT user_id FROM users WHERE login=:username AND password=:userpass"
     
-    cursor.execute(sqlcmd, {"username":username, "userpass":userpass})
+    cursor.execute(sqlcmd, {"username":username, "userpass":password})
     try:
         login_accept()
     except:
         login_deny()
-        return
 
 
 def register():
 
-    global user, cursor, connection
+    global user, role, cursor, connection
 
     printScreen('Register');
 
@@ -95,27 +193,31 @@ def register():
     while True:
         role = input("Role:\n1.Account Manager\n2.Supervisor\n3.Dispatcher\n4.Driver\n\n")
         if role == '1':
-            role == 'Account Manager'
+            role = 'Account Manager'
             break
         elif role == '2':
-            role == 'Supervisor'
+            role = 'Supervisor'
             break
         elif role == '3':
-            role == 'Dispatcher'
+            role = 'Dispatcher'
             break
         elif role == '4':
-            role == 'Driver'
+            role = 'Driver'
             break
 
     #Assign a random not already used id.
     while True:
-        user_id = randint(0,sys.maxsize)
+        user = str(randint(0,sys.maxsize))
         sqlcmd = "SELECT user_id FROM users WHERE user_id=:user_id"
-        cursor.execute(sqlcmd, {"user_id":user_id})
+        cursor.execute(sqlcmd, {"user_id":user})
         try:
             cursor.fetchone()[0]
         except:
             break
+
+    #DEBUG!!! REMOVE LATER
+    #setting user_id to an existing id in the database.
+    user = '74569'
 
     #Insert into user table
     sqlcmd = "INSERT INTO users(user_id, role, login, password) VALUES (:user_id, :role, :login, :password)"
@@ -123,6 +225,7 @@ def register():
     connection.commit()
 
     printScreen("Welcome User" + str(user_id))
+    applyRole()
 
 def main():
     
